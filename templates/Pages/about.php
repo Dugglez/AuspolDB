131634f31395784c10ccff21db021779367b49a9
Dugglez <eism0002@student.monash.edu>
2024-01-04 21:01:52 +1100

    more to about page

--- templates/Pages/about.php
+++ templates/Pages/about.php
@@ -16,6 +16,23 @@
                             timeline of state and federal elections</a>. Then the idea hit me. Hovering over each
                         election and comparing the months manually took a bit of time. If I could query a
                         database that had all this information, I could have answered it so much quicker.
+                        Not only that, but it has the advantage over Wikipedia's records (which, like AuspolDB,
+                        are sourced from Adam Carr's<a href="http://psephos.adam-carr.net/countries/a/australia/" target="_blank"> election archive</a>)
+                        that it can be searched and filtered through. Certainly, with both sites you could find all the results in an electorate,
+                        or find where a particular elected MP ran in a particular year, but it would be far more difficult
+                        to find Wilhelm Kapphan using Wikipedia than it was using AuspolDB.
+
+                        <?php
+                        echo $this->Html->link(
+                            'Wilhelm Kapphan ',
+                            ['controller' => 'Candidates', 'action' => 'view', 10315]
+                        );
+                        ?>
+                        ran in four separate Victorian divisions in the 1980 Federal election, as well as for the senate in Victoria.
+                        He was not successful in any of his five contests. It appears he
+                        <a href="https://companieshouse.com.au/the-trustee-for-wilhelm-kapphan-11453040613" target="_blank">died in 2016. </a>
+                        Why did he do this? We may never know, but at least we now know that it happened. There are
+                        sure to be more mysteries to be discovered in the election data.
                         This site was built using CakePHP 4, and the data is stored on a MySQL database.
                         If you consider yourself technically inclined, you can download the dataset if you'd like to
                         manipulate it in ways I hadn't considered, or just to run SQL statements on it. AuspolDB's

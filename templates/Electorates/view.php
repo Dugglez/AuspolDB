08d9101cab421da369cd39a4b3c7f5f1b09d3d11
Dugglez <Ezraisma31@gmail.com>
2024-01-13 19:20:22 +1100

    display multi member district count

--- templates/Electorates/view.php
+++ templates/Electorates/view.php
@@ -388,9 +388,9 @@
                                             echo 'Uncontested';
                                         } elseif ($twocp_or_majority == 99999999) {
                                             echo '2CP Unknown';
-                                        } elseif (preg_match('/^12345678/', substr($twocp_or_majority, 0, 8))) {
+                                        } elseif (preg_match('/^123456/', substr($twocp_or_majority, 0, 6))) {
                                             // Extract the 9th digit
-                                            $ninth_digit = substr($twocp_or_majority, 8, 1);
+                                            $ninth_digit = substr($twocp_or_majority, 6, 1);
                                             echo "{$ninth_digit} Member District";
                                         }
 

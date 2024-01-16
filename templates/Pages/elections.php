cf49b258af539adbb7263cc3b756f916ee655317
Dugglez <Ezraisma31@gmail.com>
2024-01-16 14:25:25 +1100

    added some links

--- templates/Pages/elections.php
+++ templates/Pages/elections.php
@@ -11,29 +11,119 @@
                     <br>    <br>
                     <h2>Federal</h2>
                     <h3>House of Representatives</h3>
-                    At the first Australian election in 1901, 65 divisions (also known as electorates or seats) elected members. Of these, 63 were single-member electorates,
-                    meaning they elected one member. The divisions of Tasmania and South Australia (which were divided into single member electorates at the next election) elected
+                    At the first Australian election in <?php
+                    echo $this->Html->link(
+                        '1901',
+                        ['controller' => 'Elections', 'action' => 'view', 46]
+                    );
+                    ?>, 65 divisions (also known as electorates or seats) elected members. Of these, 63 were single-member electorates,
+                    meaning they elected one member. The divisions of <?php
+                    echo $this->Html->link(
+                        'Tasmania',
+                        ['controller' => 'Electorates', 'action' => 'view', 217]
+                    );
+                    ?> and <?php
+                    echo $this->Html->link(
+                        'South Australia',
+                        ['controller' => 'Electorates', 'action' => 'view', 216]
+                    );
+                    ?> (which were divided into single member electorates at the next election) elected
                     five and seven members respectively. These members were elected using the First-past-the-post voting system, where a voter had one vote and the candidate with
                     the most votes was elected. Voting was restricted to men in 1901, but was expanded to include women in 1902.
                     In 1918, the Nationalist and Country parties were defeated in the Swan by-election by the Labor party. The two conservative parties
                     contesting the by-election split the conservative vote which allowed Labor to win. Subsequently, the Nationalist government introduced full preferential voting.
                     Voters were now required to number their ballot in order of preference. In 1924, voting in federal elections was made compulsory. Section 24 of the Constitution ties the number of Members of the House of Representatives to the senate.
-                    In 1949, the number of senators for each state was increased from six to ten, and the House of Representatives was expanded from 75 seats to 123 seats.
-                    In 1984, the number of senators was increased from ten to twelve, and the House of Representatives was expanded from 125 to 148 seats. Federal elections
-                    generally occur every three years, but the Governor-General can dissolve parliament earlier on the advice of the Prime Minister. The last time this happened was 1998.
+                    In <?php
+                    echo $this->Html->link(
+                        '1949',
+                        ['controller' => 'Elections', 'action' => 'view', 28]
+                    );
+                    ?>, the number of senators for each state was increased from six to ten, and the House of Representatives was expanded from 75 seats to 123 seats.
+                    In <?php
+                    echo $this->Html->link(
+                        '1984',
+                        ['controller' => 'Elections', 'action' => 'view', 13]
+                    );
+                    ?>, the number of senators was increased from ten to twelve, and the House of Representatives was expanded from 125 to 148 seats. Federal elections
+                    generally occur every three years, but the Governor-General can dissolve parliament earlier on the advice of the Prime Minister. The last time this happened was <?php
+                    echo $this->Html->link(
+                        '1998',
+                        ['controller' => 'Elections', 'action' => 'view', 8]
+                    );
+                    ?>.
                     <br>    <br>
                     <h3>Senate</h3>
-                    At the first Australian election in 1901, the six states elected six senators. Each voter had six votes (not six preferences, but six votes which were all counted),
+                    At the first Australian election in <?php
+                    echo $this->Html->link(
+                        '1901',
+                        ['controller' => 'Elections', 'action' => 'view', 46]
+                    );
+                    ?>, the six states elected six senators. Each voter had six votes (not six preferences, but six votes which were all counted),
                     and the top six candidates were elected. Of those six, the top three were elected for six year terms, and the bottom three were elected for three year terms.
                     Subsequently, only three members were elected at Senate election until 1914. Section 57 of the Constitution provides a mechanism for a government to render all
                     Senate seats vacant rather than half, if a requirement of legislative blockage by the Senate is met. This is known as a double dissolution election, and the first
-                    double dissolution election was held in 1914. Six other double dissolution elections were held in 1951, 1974, 1975, 1983, 1987 and 2016. In 1949, the Senate was
+                    double dissolution election was held in <?php
+                    echo $this->Html->link(
+                        '1914',
+                        ['controller' => 'Elections', 'action' => 'view', 41]
+                    );
+                    ?>. Six other double dissolution elections were held in <?php
+                    echo $this->Html->link(
+                        '1951',
+                        ['controller' => 'Elections', 'action' => 'view', 27]
+                    );
+                    ?>, <?php
+                    echo $this->Html->link(
+                        '1974',
+                        ['controller' => 'Elections', 'action' => 'view', 18]
+                    );
+                    ?>, <?php
+                    echo $this->Html->link(
+                        '1975',
+                        ['controller' => 'Elections', 'action' => 'view', 17]
+                    );
+                    ?>, <?php
+                    echo $this->Html->link(
+                        '1983',
+                        ['controller' => 'Elections', 'action' => 'view', 14]
+                    );
+                    ?>, <?php
+                    echo $this->Html->link(
+                        '1987',
+                        ['controller' => 'Elections', 'action' => 'view', 12]
+                    );
+                    ?> and <?php
+                    echo $this->Html->link(
+                        '2016',
+                        ['controller' => 'Elections', 'action' => 'view', 2]
+                    );
+                    ?>. In <?php
+                    echo $this->Html->link(
+                        '1949',
+                        ['controller' => 'Elections', 'action' => 'view', 28]
+                    );
+                    ?>, the Senate was
                     expanded from six seats per state to ten. The voting system was also changed from the block voting system (which produced disproportionate results) to the
                     Single Transferable Vote system. Senators were now elected with a quota of the votes, rather than candidates with the most votes being elected.
                     Generally, Senate elections are held on the same day as House of Representatives elections. Between 1951 and 1970, however, the two cycles of the two houses
-                    were put out of sync. The 1953 Senate election was the first to be held without a House of Representatives election, and the 1970 Senate election was the last.
-                    In 1974, four senator, two from the ACT and two from the NT were elected for the first time, following legislation which gave them Senate representation for the first time.
-                    In 1984, the Senate was expanded from ten to twelve senators per state. At the same time, the Hawke Government also introduced the Group Voting Ticket system. Under
+                    were put out of sync. The <?php
+                    echo $this->Html->link(
+                        '1953',
+                        ['controller' => 'Elections', 'action' => 'view', 50]
+                    );
+                    ?> Senate election was the first to be held without a House of Representatives election, and the <?php
+                    echo $this->Html->link(
+                        '1970',
+                        ['controller' => 'Elections', 'action' => 'view', 47]
+                    );
+                    ?> Senate election was the last.
+                    In 1974, four senators, two from the ACT and two from the NT were elected for the first time, following legislation which gave them Senate representation for the first time.
+                    In <?php
+                    echo $this->Html->link(
+                        '1984',
+                        ['controller' => 'Elections', 'action' => 'view', 13]
+                    );
+                    ?>, the Senate was expanded from ten to twelve senators per state. At the same time, the Hawke Government also introduced the Group Voting Ticket system. Under
                     the system, voters could choose to either nominate preferences for every candidate using the bottom half of the ballot (which was becoming more difficult as more parties began to contest the Senate),
                     or number one box on the top half of the ballot, which would have their preferences nominated by their preferred party on their behalf based on a registered ticket.
                     Rates of informality (or invalid votes) decreased, however the system led to some candidates with low primary votes but effective negotiating skills being elected.
@@ -51,8 +141,18 @@
                     Prior to 1870, Members of the Victorian Parliament were not paid a salary.
                     <br>    <br>
                     <h3>Legislative Council</h3>
-                    The First Victorian Legislative Council, an upper house like the later Federal Senate, was convened in 1851, with some members being appointed and others being elected.
-                    The Second Victorian Legislative Council was wholly elected in 1856, following the Constitution of Victoria receiving royal assent.
+                    The First Victorian Legislative Council, an upper house like the later Federal Senate, was convened in <?php
+                    echo $this->Html->link(
+                        '1851',
+                        ['controller' => 'Elections', 'action' => 'view', 114]
+                    );
+                    ?>, with some members being appointed and others being elected.
+                    The Second Victorian Legislative Council was wholly elected in <?php
+                    echo $this->Html->link(
+                        '1856',
+                        ['controller' => 'Elections', 'action' => 'view', 117]
+                    );
+                    ?>, following the Constitution of Victoria receiving royal assent.
                     5 members for each of the six provinces of Victoria were elected. Terms lasted ten years, with the candidate at the first election
                     with the most votes receiving a ten-year term, while the candidate with the fifth-most votes received a two-year term.
                     Voting for the Legislative Council was restricted to men over the age of 21 who owned over £1000 of property.
@@ -62,10 +162,25 @@
                     two members.
                     In 1922, following Federal reform, preferential voting was implemented.
                     In 1937, voting became compulsory for those who met the property ownership requirement.
-                    In 1950, the Country/Labor coalition government abolished the requirement of property ownership. At the election prior to the reform,
-                    nine Liberal members were elected to Labor's four. At the election after the reform, the Liberals returned one member to Labor's eleven.
+                    In 1950, the Country/Labor coalition government abolished the requirement of property ownership. At the <?php
+                    echo $this->Html->link(
+                        'election',
+                        ['controller' => 'Elections', 'action' => 'view', 159]
+                    );
+                    ?> prior to the reform,
+                    nine Liberal members were elected to Labor's four. At the <?php
+                    echo $this->Html->link(
+                        'election',
+                        ['controller' => 'Elections', 'action' => 'view', 160]
+                    );
+                    ?> after the reform, the Liberals returned one member to Labor's eleven.
                     Prior to 1961, Legislative council elections were held every two, and then every three, years, out of sync with the Legislative Assembly. Since
-                    1961, both houses have been elected on the same day.
+                    <?php
+                    echo $this->Html->link(
+                        '1961',
+                        ['controller' => 'Elections', 'action' => 'view', 69]
+                    );
+                    ?>, both houses have been elected on the same day.
                     In 1962, the franchise for the Legislative Council and Legislative Assembly was extended to all adults, regardless of race or gender.
                     In 1988, following federal reform in 1984, the Cain government implemented the Group Voting Ticket system. Under
                     the system, voters could choose to either nominate preferences for every candidate using the bottom half of the ballot (which was becoming more difficult as more parties began to contest the Senate),
@@ -82,7 +197,12 @@
                     <br>    <br>
                     <h3>Legislative Assembly</h3>
                     The first election of the Victorian Legislative Assembly, a lower house like the federal House of Representatives,
-                    was held in 1856. Unlike the Legislative Council, voting for the Legislative Assembly was open to all men over 21
+                    was held in <?php
+                    echo $this->Html->link(
+                        '1856',
+                        ['controller' => 'Elections', 'action' => 'view', 113]
+                    );
+                    ?>. Unlike the Legislative Council, voting for the Legislative Assembly was open to all men over 21
                     in 1857, regardless of property ownership. The Parliament of Victoria website adds that "[t]his included Aboriginal and Torres Strait Islander men,
                     though voter suppression prevented most from using this right".
                     According to <a href="http://psephos.adam-carr.net/countries/a/australia/states/vic/historic/intro.txt" target="_blank">Adam Carr</a>, until 1859
